77227fc960b669e1fb5b7d3ccb557498ebd748c4
once-aday <onceaday@gmail.com>
2020-08-26 13:27:26 -0700

    adjust duplicate-shift script to add line shifts properly in while loop

--- duplicate-and-shift-server/duplicate-shift.js
+++ duplicate-and-shift-server/duplicate-shift.js
@@ -5,21 +5,22 @@ const outFilePath = "data/duplicated-lines.json";
 // How many duplicates?
 var duplicates = 5
 
-// How many meters to shift each successive line?
+// How many kilometers to shift each successive line? (Default is Kilometers)
 var shiftMeters = 10
 
 // What Direction should they shift? 0.0 = north, 180.0 = south
 const shiftDirection = 0.0
 
 var collectedFeatures = []
+var shift = 0
 
 while (duplicates > 0) {
   // text += "The number is " + duplicates;
   var line = turf.lineString([[-74, 40], [-78, 42], [-82, 35]], {color: 'red'});
   var lineCloned = turf.clone(line);
-  var translatedLine = turf.transformTranslate(lineCloned, shiftMeters, shiftDirection);
+  var translatedLine = turf.transformTranslate(lineCloned, shift, shiftDirection);
   collectedFeatures.push(translatedLine);
-  shiftMeters += 10
+  shift += shiftMeters
   duplicates--;
 }
 
